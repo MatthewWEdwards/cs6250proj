1f346e3776c125a3f934ffb070d59d465f9fa487
Matthew Edwards <contact@matthewedwards.io>
2021-07-27 00:36:24 -0400

    Add AS to schema. Switches.

--- json_topo.py
+++ json_topo.py
@@ -4,7 +4,7 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info, debug
-from mininet.node import Host, RemoteController, OVSSwitch
+from mininet.node import Host, OVSSwitch
 
 import json
 import os
@@ -17,6 +17,29 @@ CONFIG_DIR = 'configs/'
 ZEBRA_CMD = "/usr/lib/quagga/zebra"
 BGPD_CMD = "/usr/lib/quagga/bgpd"
 
+class Controller(Host):
+
+    def __init__(self, name, intfDict, *args, **kwargs):
+        Host.__init__(self, name, *args, **kwargs)
+
+        self.intfDict = intfDict
+
+    def config(self, **kwargs):
+        Host.config(self, **kwargs)
+
+        for intf, attrs in self.intfDict.items():
+            self.cmd('ip addr flush dev %s' % intf)
+            if 'mac' in attrs:
+                self.cmd('ip link set %s down' % intf)
+                self.cmd('ip link set %s address %s' % (intf, attrs['mac']))
+                self.cmd('ip link set %s up ' % intf)
+            for addr in attrs['ipAddrs']:
+                self.cmd('ip addr add %s dev %s' % (addr, intf))
+
+class L2Switch(OVSSwitch):
+
+    def start(self, controllers):
+        return OVSSwitch.start(self, [])
 
 class QuaggaRouter(Host):
 
@@ -91,40 +114,59 @@ class L2Switch(OVSSwitch):
 
 class Topo(Topo):
 
-    def build(self, topopath):
+    def build(self):
+        topopath = "./topos/small_topo.json"
         topo = json.loads(open(topopath, "r").read())
+        
+        for as_name, as_dict in topo["ASes"].items():
+            routers, hosts, exabgps = {}, {}, {}
 
-        # Add routers and BGP monitors
-        routers = {}
-        for router_name, router_attrs in topo["routers"].items():
-            zebraConf = f"%s{router_attrs['zebra']}" % CONFIG_DIR
-            quaggaConf = f"%s{router_attrs['quagga']}" % CONFIG_DIR
-            r = self.addHost(router_name, cls=QuaggaRouter, quaggaConfFile=quaggaConf,
-                    zebraConfFile=zebraConf, intfDict=router_attrs["netifs"])
-            routers[router_name] = r
+            # Add routers and BGP monitors
+            for router_name, router_attrs in as_dict["routers"].items():
+                zebraConf = f"%s{router_attrs['zebra']}" % CONFIG_DIR
+                quaggaConf = f"%s{router_attrs['quagga']}" % CONFIG_DIR
+                r = self.addHost(router_name, cls=QuaggaRouter, quaggaConfFile=quaggaConf,
+                        zebraConfFile=zebraConf, intfDict=router_attrs["netifs"])
+                routers[r] = router_attrs
 
-            if "bgp" in router_attrs.keys():
-                # Set up BGP monitors
-                name = 'exabgp'
-                exabgp = self.addHost(name, cls=ExaBGPRouter,
-                                      exaBGPconf='%sexabgp.conf' % CONFIG_DIR,
-                                      intfDict=router_attrs["bgp"]["netifs"])
+                if "bgp" in as_dict.keys():
+                    # Set up BGP monitors
+                    name = f"{as_name}-exabgp"
+                    exabgp = self.addHost(name, cls=ExaBGPRouter,
+                                          exaBGPconf='%sexabgp.conf' % CONFIG_DIR,
+                                          intfDict=as_dict["bgp"]["netifs"])
+                    exabgps[name] = as_dict["bgp"]
 
-                bgplink = router_attrs["bgp"]["link"]
-                self.addLink(r, exabgp, port1=bgplink["router_port"], port2=bgplink["exabgp_port"])
+            # Add hosts
+            hosts = {}
+            for host_name, host_attrs in as_dict["hosts"].items():
+                h = self.addHost(host_name, 
+                        ip=host_attrs["ip"], 
+                        defaultRoute=f"via {host_attrs['defaultRoute']}")
+                hosts[host_name] = host_attrs
+
+            # Add switch
+            linkcnt = 1
+            l2switch = self.addSwitch(f"{as_name}-l2switch", failMode="standalone", dpid="0000000000000001", cls=L2Switch)
+            for devtype in [routers, exabgps, hosts]:
+                for name, attrs in devtype.items():
+                    if "switch" in attrs.keys():
+                        self.addLink(l2switch, name, port1=linkcnt, port2=attrs["switch"])
+                        linkcnt += 1
+        # Controller
+        self.addHost("controller", cls=Controller, intfDict=topo["controller"]["netifs"])
+
+        # Inter-AS connections
+        for link in topo["links"]:
+            self.addLink(link["dev1"], link["dev2"], link["port1"], link["port2"])
 
 
-        # Add hosts
-        hosts = {}
-        for host_name, host_attrs in topo["hosts"].items():
-            h = self.addHost(host_name, ip=host_attrs["ip"], defaultRoute=f"via {host_attrs['defaultRoute']}")
-            hosts[host_name] = h
 
 topos = {'smalltopo': Topo}
 
 if __name__ == '__main__':
     setLogLevel('debug')
-    topo = Topo("./topos/small_topo.json")
+    topo = Topo()
 
     net = Mininet(topo=topo, build=False)
     net.build()
